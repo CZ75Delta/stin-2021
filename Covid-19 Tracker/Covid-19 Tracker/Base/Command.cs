077473175f507a45c60ee2f9babcecb398ce55c2
Vít Škrbel <vitek.skrbel@gmail.com>
2021-04-28 12:01:56 +0200

    Added MVVM Scaffolding

---
+++ Covid-19 Tracker/Covid-19 Tracker/Base/Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Covid_19_Tracker.Base
+{
+    class Command : ICommand
+    {
+        private readonly Action _execute;
+
+        public Command(Action execute)
+        {
+            _execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+}
---
+++ Covid-19 Tracker/Covid-19 Tracker/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Covid_19_Tracker.Base;
+
+namespace Covid_19_Tracker.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region Global Variables
+ 
+        #endregion
+
+        #region Bindable Properties
+
+        #endregion
+
+        #region Command Declarations
+
+        #endregion
+
+        #region Command Methods
+
+        #endregion
+
+        public new void OnPropertyChanged(string propertyName) { base.OnPropertyChanged(propertyName); }
+    }
+}
