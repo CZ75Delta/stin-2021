4d12dbff1d7635e04239aaed2cae46a507b55d31
Vít Škrbel <vitek.skrbel@gmail.com>
2021-04-28 12:07:51 +0200

    Added EFCore database context class.

---
+++ Covid-19 Tracker/Covid-19 Tracker/Model/DbContext/TrackerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Covid_19_Tracker.Model.DbContext
+{
+    public class TrackerDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=TrackerDb.db");
+    }
+}
