6c01dc463d0cae2739d65d8f8c9ffb8c789dee65
Seivercz <55312097+Seivercz@users.noreply.github.com>
2021-05-13 00:40:03 +0200

    Pokus o http requester

---
+++ Covid-19 Tracker/Covid-19 Tracker/Model/HttpRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Covid_19_Tracker.Model
+{
+    public class HttpRequest
+    {
+        public string Data { get; }
+
+        private HttpRequest(string url)
+        {
+            using var webClient = new WebClient();
+            Data = string.Empty;
+            try
+            {
+                Data = webClient.DownloadString(url);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            
+        }
+    }
+}
