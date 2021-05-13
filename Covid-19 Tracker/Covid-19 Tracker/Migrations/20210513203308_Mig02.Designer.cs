78bc28dacebd0fcedff43f0ca7f90ed63b047baf
Vit_Skrbel <vit.skrbel@tul.cz>
2021-05-13 22:39:56 +0200

    Addded ISO Code to Country class

--- Covid-19 Tracker/Covid-19 Tracker/Migrations/20210513203308_Mig02.Designer.cs
+++ Covid-19 Tracker/Covid-19 Tracker/Migrations/20210513203308_Mig02.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Covid_19_Tracker.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Covid_19_Tracker.Migrations
 {
     [DbContext(typeof(TrackerDbContext))]
-    partial class TrackerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210513203308_Mig02")]
+    partial class Mig02
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Covid-19 Tracker/Covid-19 Tracker/Model/Country.cs
+++ Covid-19 Tracker/Covid-19 Tracker/Model/Country.cs
@@ -6,6 +6,7 @@ namespace Covid_19_Tracker.Model
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string IsoCode { get; set; }
         public int Population { get; set; }
         public List<Infected> Infected { get; set; }
         public List<Vaccinated> Vaccinated { get; set; }
