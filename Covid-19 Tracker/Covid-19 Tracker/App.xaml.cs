35dbfe9e0914b2f5eebfff71b9ac833000672359
Vít Škrbel <vitek.skrbel@gmail.com>
2021-05-20 19:13:21 +0200

    Implemented automatic internet connection checks

--- Covid-19 Tracker/Covid-19 Tracker/App.xaml.cs
+++ Covid-19 Tracker/Covid-19 Tracker/App.xaml.cs
@@ -18,16 +18,6 @@ namespace Covid_19_Tracker
                 await using var ctx = new TrackerDbContext();
                 await ctx.Database.MigrateAsync();
                 await ctx.SaveChangesAsync();
-
-                //Update on startup
-                var apiHandler = new ApiHandler();
-                var processData = new ProcessData();
-                var dataToDb = new DataToDb();
-                var listWho = processData.CSVToListWHOCountries(apiHandler.DownloadFromUrl("https://covid19.who.int/who-data/vaccination-data.csv").Result).Result;
-                await dataToDb.InitializeCountries(listWho);
-                await dataToDb.SavetoDb(listWho);
-                await dataToDb.SavetoDb(processData.JSONToDictMZCR(apiHandler.DownloadFromUrl("https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/zakladni-prehled.json").Result).Result);
-                await dataToDb.SavetoDb(processData.CSVToDictWHOCR(apiHandler.DownloadFromUrl("https://covid19.who.int/WHO-COVID-19-global-data.csv").Result).Result);
             });
         }
     }
--- Covid-19 Tracker/Covid-19 Tracker/ViewModel/MainViewModel.cs
+++ Covid-19 Tracker/Covid-19 Tracker/ViewModel/MainViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using System.Threading;
+using System.Timers;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 using Covid_19_Tracker.Base;
 using Covid_19_Tracker.Model;
 
@@ -16,16 +18,19 @@ namespace Covid_19_Tracker.ViewModel
         private readonly ProcessData _processData;
         private readonly DataToDb _dataToDb;
         private readonly CheckInternetConnection _checkInternet;
-
+        private Timer _updateTimer;
+        private DispatcherTimer _retryTextTimer;
+        private int _retrySeconds;
         private string _progressText;
-        private int _progressBar;
+        private bool _progressBar;
+        private DateTime _lastUpdate;
 
         #endregion
 
         #region Bindable Properties
 
         public string ProgressText { get => _progressText; private set { _progressText = value; OnPropertyChanged(); } }
-        public int ProgressBar { get => _progressBar; private set { _progressBar = value; OnPropertyChanged(); } }
+        public bool ProgressBar { get => _progressBar; private set { _progressBar = value; OnPropertyChanged(); } }
 
 
         #endregion
@@ -42,33 +47,32 @@ namespace Covid_19_Tracker.ViewModel
         {
             if (await _checkInternet.CheckForInternetConnection(1000))
             {
+                _lastUpdate = DateTime.Now;
                 await Task.Factory.StartNew(async () =>
                 {
-                    ProgressBar = 0;
+                    ProgressBar = true;
 
                     //GET WHO Vaccinations
                     var listWho = _processData.CSVToListWHOCountries(_apiHandler.DownloadFromUrl("https://covid19.who.int/who-data/vaccination-data.csv").Result).Result;
                     await _dataToDb.InitializeCountries(listWho);
-                    ProgressBar = 20;
 
                     await _dataToDb.SavetoDb(listWho);
-                    ProgressBar = 40;
 
                     //GET MZČR Summary
                     await _dataToDb.SavetoDb(_processData.JSONToDictMZCR(_apiHandler.DownloadFromUrl("https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/zakladni-prehled.json").Result).Result);
-                    ProgressBar = 60;
 
                     //GET WHO Infections
                     await _dataToDb.SavetoDb(_processData.CSVToDictWHOCR(_apiHandler.DownloadFromUrl("https://covid19.who.int/WHO-COVID-19-global-data.csv").Result).Result);
-                    ProgressBar = 80;
 
-                    ProgressText = "Naposledy aktualizováno v " + DateTime.Now.ToString("HH:mm");
+                    ProgressText = "Poslední aktualizace v " + _lastUpdate.ToString("HH:mm");
                 });
             }
             else
             {
-                ProgressText = "Nelze se připojit k internetu.";
+                SetRetryTextTimer();
             }
+
+            ProgressBar = false;
         }
 
         #endregion
@@ -80,7 +84,40 @@ namespace Covid_19_Tracker.ViewModel
             _processData = new ProcessData();
             _checkInternet = new CheckInternetConnection();
             RefreshCommand = new Command(UpdateData);
-            ProgressText = "Naposledy aktualizováno v " + DateTime.Now.ToString("HH:mm");
+            ProgressText = "Poslední aktualizace v " + DateTime.Now.ToString("HH:mm");
+            SetUpdateTimer(600000);
+            UpdateData();
+        }
+
+        private void SetRetryTextTimer()
+        {
+            _retryTextTimer = new DispatcherTimer {Interval = TimeSpan.FromMilliseconds(1000)};
+            _retryTextTimer.Tick += SetRetryTextTimerTick;
+            _retryTextTimer.Start();
+            _retrySeconds = 30;
+            ProgressText = "Nelze se připojit k internetu, zkouším znovu za " + _retrySeconds + "s";
+        }
+
+        private void SetRetryTextTimerTick(object sender, EventArgs e)
+        {
+            _retrySeconds -= 1;
+            ProgressText = "Nelze se připojit k internetu, zkouším znovu za " + _retrySeconds + "s";
+            if (_retrySeconds != 0) return;
+            _retryTextTimer.Stop();
+            UpdateData();
+        }
+
+        private void SetUpdateTimer(double interval)
+        {
+            _updateTimer = new Timer(interval);
+            _updateTimer.Elapsed += UpdateDataEvent;
+            _updateTimer.AutoReset = true;
+            _updateTimer.Enabled = true;
+        }
+
+        private void UpdateDataEvent(object sender, ElapsedEventArgs e)
+        {
+            UpdateData();
         }
 
         private new void OnPropertyChanged([CallerMemberName] string propertyName = "") { base.OnPropertyChanged(propertyName); }
