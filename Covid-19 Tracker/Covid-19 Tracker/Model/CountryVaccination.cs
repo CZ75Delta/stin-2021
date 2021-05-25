6fc51763ba70309e8d7999f392c6a3cedc448071
Petr_Schonherr <petr.schonherr@tul.cz>
2021-05-25 21:24:37 +0200

    Start of nothing

---
+++ Covid-19 Tracker/Covid-19 Tracker/Model/CountryVaccination.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid_19_Tracker.Model
+{
+    class CountryVaccination
+    {
+        public readonly string Name;
+        public bool IsPicked { get; set; }
+        public readonly int VaccinatedCounter;
+        public readonly string VaccinatedPercent;
+
+        private CountryVaccination(Country c)
+        {
+            Name = c.Name;
+
+        }
+
+    }
+}
