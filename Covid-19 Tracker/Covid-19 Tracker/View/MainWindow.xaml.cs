85fe95a2edb3bc5799858e7d83e3cce00f42e539
Tomas_Novotny <tomas.novotny@tul.cz>
2021-05-30 00:26:14 +0200

    přidaná fronta pro výběr zemí

--- Covid-19 Tracker/Covid-19 Tracker/View/MainWindow.xaml.cs
+++ Covid-19 Tracker/Covid-19 Tracker/View/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using Covid_19_Tracker.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace Covid_19_Tracker.View
 {
@@ -9,9 +14,13 @@ namespace Covid_19_Tracker.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Queue<CountryVaccination> VaccinationQueue { get; private set; }
+
         public MainWindow()
         {
+            VaccinationQueue = new Queue<CountryVaccination>();            
             InitializeComponent();
+            
         }
 
         private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -24,21 +33,26 @@ namespace Covid_19_Tracker.View
         {
             if (e.AddedItems.Count > 0)
             {
-                int counter = DataGridPickedCountries.Items.Count;
-                var selectedCountry = (CountryVaccination)e.AddedItems[0];
+                var selectedCountry = (CountryVaccination)e.AddedItems[0];                
 
                 if (selectedCountry.IsPicked == true)
                 {
+                    VaccinationQueue = new Queue<CountryVaccination>(VaccinationQueue.Where(s => s != selectedCountry));
                     selectedCountry.IsPicked = false;
-                    counter--;
                 }
                 else
                 {
-                    if (counter <= 3)
+                    if (VaccinationQueue.Count >= 4)
+                    {
+                        CountryVaccination endOfQueue = VaccinationQueue.Dequeue();
+                        endOfQueue.IsPicked = false;
+                    }
+                    if (VaccinationQueue.Count <= 3)
                     {
                         selectedCountry.IsPicked = true;
-                        counter++;
+                        VaccinationQueue.Enqueue(selectedCountry);
                     }
+                    
                 }
             }
         }
