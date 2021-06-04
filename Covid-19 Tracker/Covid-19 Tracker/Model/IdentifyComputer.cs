56c5b8a8a039b3f59b9db23f0f12902755f1969b
Petr_Schonherr <petr.schonherr@tul.cz>
2021-06-04 12:01:59 +0200

    IdentifyComputer added Try Catch

--- Covid-19 Tracker/Covid-19 Tracker/Model/IdentifyComputer.cs
+++ Covid-19 Tracker/Covid-19 Tracker/Model/IdentifyComputer.cs
@@ -8,10 +8,17 @@ namespace Covid_19_Tracker.Model
     {
         public static async Task<string> GetIdentification()
         {
-            using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
-            await socket.ConnectAsync("8.8.8.8", 65530);
-            var endPoint = socket.LocalEndPoint as IPEndPoint;
-            return new WebClient().DownloadString("https://ipinfo.io/ip").Replace("\n", "") + "/" + endPoint?.Address;
+            try
+            {
+                using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
+                await socket.ConnectAsync("8.8.8.8", 65530);
+                var endPoint = socket.LocalEndPoint as IPEndPoint;
+                return new WebClient().DownloadString("https://ipinfo.io/ip").Replace("\n", "") + "/" + endPoint?.Address;
+            }
+            catch
+            {
+                return "Failed to get IP";
+            }
         }
     } 
 }
