c90690c2ce1f33ea5be224c938cefb248abfe488
Vít Škrbel <vitek.skrbel@gmail.com>
2021-06-07 15:37:32 +0200

    Started on firewall rules.

--- Covid-19 Tracker/Covid-19 Tracker/App.xaml.cs
+++ Covid-19 Tracker/Covid-19 Tracker/App.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.Resources;
+using System.Windows;
 using Covid_19_Tracker.Model;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using NetFwTypeLib;
 
 namespace Covid_19_Tracker
 {
@@ -12,14 +15,34 @@ namespace Covid_19_Tracker
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 10,
                     fileSizeLimitBytes: 52428800, rollOnFileSizeLimit: true)
-                .WriteTo.Telegram(
-                    "1896853074:AAGadAOmXiE90sTPlsxEyniV-f0WnU1CKlA", "-500972830",
+                .WriteTo.Telegram(Covid_19_Tracker.Properties.Resources.TelegramApiKey, Covid_19_Tracker.Properties.Resources.TelegramChatId,
                     applicationName: IdentifyComputer.GetIdentification().Result)
                 .CreateLogger();
             Log.Information("Application started.");
+
+            try
+            {
+                var firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2") ?? throw new InvalidOperationException());
+                var firewallRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule") ?? throw new InvalidOperationException());
+                if (firewallRule != null)
+                {
+                    firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_ALLOW;
+                    firewallRule.Description = "Allows the Covid-19 tracker to access the internet.";
+                    firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_IN; // inbound
+                    firewallRule.Enabled = true;
+                    firewallRule.InterfaceTypes = "All";
+                    firewallRule.Name = "Covid-19 Tracker";
+                    firewallPolicy?.Rules.Add(firewallRule);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Application could not add firewall rule. " + ex.Message);
+            }
             using var ctx = new TrackerDbContext();
             ctx.Database.Migrate();
             ctx.SaveChanges();
